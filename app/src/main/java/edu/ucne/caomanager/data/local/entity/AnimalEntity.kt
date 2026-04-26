89dc8c50cf6d0e6f277e294fb7093b8c6d138677
STIVEN JOSE SANTANA CRUZ <STIVEN_SANTANA@ucne.edu.do>
2026-04-26 15:38:23 -0400

    Crear entidad AnimalEntity para Room

---
+++ app/src/main/java/edu/ucne/caomanager/data/local/entity/AnimalEntity.kt
@@ -0,0 +1,15 @@
+package edu.ucne.caomanager.data.local.entity
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "animales")
+data class AnimalEntity(
+    @PrimaryKey(autoGenerate = true)
+    val idAnimal: Int = 0,
+    val codigo: String,
+    val fechaNacimiento: String?,
+    val pesoInicial: Double,
+    val precioCompra: Double,
+    val estado: String
+)
